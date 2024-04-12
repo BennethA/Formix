c85d0abde2d3c48f686eeef2c6af5ef4ce7d9d08
Benneth Addo <142854946+BennethA@users.noreply.github.com>
2024-04-12 12:09:50 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { FaCircle, FaStar } from 'react-icons/fa'
+import { useState } from 'react'
 import FirstPage from './FirstPage'
 import SecondPage from './SecondPage'
 import ThirdPage from './ThirdPage'
 import FourthPage from './FourthPage'
 import Image from './assets/image.jpg'
-import { useState } from 'react'
+import { FaCircle, FaStar } from 'react-icons/fa'
 import { FaCircleDot } from 'react-icons/fa6'
 
 function App() {
@@ -35,28 +35,35 @@ function App() {
   return (
     <>
       <div className="flex flex-col md:flex-row min-h-svh justify-center overflow-auto"> 
-        <div className="bg-black min-h-svh text-white p-5 md:w-1/3" >
-          <div className="font-extrabold text-lg mb-12 mt-10">Formix.</div>
-          <div className="text-4xl font-bold leading-10 mb-7">Let's formalize your operating agreement</div>
-          <div className="leading-6 mb-12">
+        <div className="bg-[#080808fb] min-h-svh text-white p-[60px] md:w-[39%]" >
+          <div className="font-medium text-3xl mb-[55px] mt-10 font-serif">Formix.</div>
+          <div className="text-5xl font-bold mb-12 leading-[60px]">Let's formalize your operating agreement</div>
+          <div className="leading-7 mb-[110px] text-2xl font-semibold text-gray-300">
             The faster you finish the operating agreement process, the sooner customers can access your profile
           </div>
-          <div className="bg-gradient-to-b from-[#1f1f1f] to-[#050505] p-5 rounded-lg mt-10">
-            <div className="mb-3 flex">
+          <div className="bg-gradient-to-b from-[#3a4644b0] to-[#222726b0] p-5 rounded-lg mt-10">
+            <div className="mb-4 flex">
               {stars.map((star, index) => <FaStar className='mr-3' key={index}/>)}
             </div>
             <div className="text-sm">
-              The business product is seriously a game change. It's got these awesome features that just make our daily grind so much easier, and it fits right into our qorkflow
+              <em>The business product is seriously a game change. It's got these awesome features that just make our daily grind so much easier, and it fits right into our workflow</em>
             </div>
-            <div className='flex mt-5 items-center flex-wrap'>
-              <img src={Image} alt="" className='w-12 rounded-full mr-3'/>
-              <p className='font-bold'>David Garrex</p>
+            <div className='flex mt-7 items-center flex-wrap'>
+              <img src={Image} alt="" className='w-[60px] rounded-full mr-3'/>
+              <p className='font-normal'>David Gorrex</p>
             </div>
           </div>
         </div>
-        <div className=' p-5 w-full min-h-svh'>
-          <div className='flex justify-around mt-10'>
-            {steps.map((step, index) => <FaCircleDot className={`${currentComponent === index + 1 ? 'text-black' : 'text-gray-400'}`} key={index}/>)}
+        <div className='py-[60px] px-[40px] pr-[65px] w-full md:w-[61%] min-h-svh'>
+          <div className='flex justify-between mt-10'>
+            {
+              steps.map((step, index) => <>
+                <FaCircle className={`transition-all border-[2.23px] rounded-xl p-[3px] text-2xl ${currentComponent > index  ? 'text-black border-black' : 'text-gray-300 border-gray-300 '}`} key={index}/>
+                {index < steps.length - 1 && (
+                  <hr className={` transition-all flex-1 h-1 m-2 ${currentComponent > index + 1 ? 'bg-black' : 'bg-gray-500'}`}></hr>
+                )}
+              </>)
+            }
           </div>
           {currentComponent === 1 && <FirstPage data={data} setData={setData} handleFirstComponent={handleFirstComponent}/>}
           {currentComponent === 2 && <SecondPage/>}
@@ -65,10 +72,10 @@ function App() {
           <div className="flex justify-between">
             {
               currentComponent > 1 
-              ? <button className="font-bold p-2 px-5 hover:bg-gray-500 rounded-2xl" onClick={handlePrevStep}>Previous step</button>
+              ? <button className=" font-semibold p-2 hover:bg-gray-500 hover:text-white text-xl font-sans text-gray-700 py-3 px-10 rounded-3xl" onClick={handlePrevStep}>Previous step</button>
               : ''
             }
-            <button className="font-bold p-2 px-5 hover:bg-gray-500 rounded-2xl" onClick={handleNextStep}>Skip step</button>
+            <button className="p-2 font-semibold hover:bg-gray-500 hover:text-white text-xl font-sans text-gray-700 py-3 px-10 rounded-3xl" onClick={handleNextStep}>Skip step</button>
             <button onClick={
               () => {
                 if(currentComponent === 1) {
@@ -77,7 +84,7 @@ function App() {
                 } else if(currentComponent === 2 || currentComponent === 3) {
                   handleNextStep()
                 }
-              }} className="font-bold bg-black text-white p-2 px-5 hover:bg-gray-500 rounded-2xl">Next step</button>
+              }} className="text-xl  font-semibold font-sans bg-black text-white py-3 px-10 hover:bg-gray-500 rounded-3xl">Next step</button>
           </div>
         </div>
       </div>
--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -3,70 +3,70 @@ import React from 'react'
 const FirstPage = ({data, setData, handleFirstComponent}) => {
   return (
     <div>
-      <div className="font-bold text-2xl mt-10 mb-5">Let's get started!</div>
+      <div className="font-semibold text-[35px] mt-14 mb-5">Let's get started!</div>
       <div>
         <form onSubmit={handleFirstComponent}>
           <div className="mb-20">
-            <div className="flex mb-4 justify-between space-x-4">
-              <div className="flex flex-col w-1/2">
-                <label htmlFor="firstName" className="text-gray-700 mb-2">
+            <div className="flex mb-8 justify-between">
+              <div className="flex flex-col w-1/2 pr-4">
+                <label htmlFor="firstName" className="text-[#818181] mb-2 text-[18px] font-semibold">
                   First Name
                 </label> 
                 <input type="text" 
-                placeholder="firstname" 
-                className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+                placeholder="firstname"
+                className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
                 onChange={(e) => setData({...data, firstName: e.target.value})}/>
               </div>
               <div className="flex flex-col w-1/2">
-                <label htmlFor="surname" className="text-gray-700 mb-2">
+                <label htmlFor="surname" className="text-[#818181] mb-2 text-[18px] font-semibold">
                   Surname
                 </label>
                 <input 
                 type="text" 
-                placeholder="surname" className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+                placeholder="surname" className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
                 onChange={(e) => setData({...data, surname: e.target.value})}/>
               </div>
             </div>
-            <div className="flex flex-col mb-4">
-              <label htmlFor="email" className="text-gray-700 mb-2">
+            <div className="flex flex-col mb-8">
+              <label htmlFor="email" className="text-[#818181] mb-2 text-[18px] font-semibold">
                 Email
               </label>
               <input 
               type="email" 
               placeholder="email" 
-              className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+              className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
               onChange={(e) => setData({...data, email: e.target.value})}/>
             </div>
-            <div className="flex mb-4 justify-between space-x-4">
-              <div className="flex flex-col w-1/2">
-                <label htmlFor="country" className="text-gray-700 mb-2">
+            <div className="flex mb-8 justify-between">
+              <div className="flex flex-col w-1/2 pr-4">
+                <label htmlFor="country" className="text-[#818181] mb-2 text-[18px] font-semibold">
                   Country
                 </label>
                 <input 
                 type="text" 
                 placeholder="country" 
-                className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+                className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
                 onChange={(e) => setData({...data, country: e.target.value})}/>
               </div>
               <div className="flex flex-col w-1/2">
-                <label htmlFor="region" className="text-gray-700 mb-2">
+                <label htmlFor="region" className="text-[#818181] mb-2 text-[18px] font-semibold">
                   Region
                 </label>
                 <input 
                 type="text" 
                 placeholder="region" 
-                className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+                className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
                 onChange={(e) => setData({...data, region: e.target.value})}/>
               </div>
             </div>
             <div className="flex flex-col">
-              <label htmlFor="phoneNumber" className="text-gray-700 mb-2">
+              <label htmlFor="phoneNumber" className="text-[#818181] mb-2 text-[18px] font-semibold">
                 Phone Number
               </label>
               <input 
               type="number" 
               placeholder="phone" 
-              className="p-3 border-[0.5px] border-gray-500 rounded-lg text-gray-950 font-semibold"
+              className="py-2 px-4 border-gray-400 border-[2px] rounded-lg text-gray-800 font-medium text-[22px]"
               onChange={(e) => setData({...data, number: e.target.value})}/>
             </div>
           </div>
