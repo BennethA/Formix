171c20728d883693535d4c39caa28db4d7bfacb3
Benneth Addo <addobenneth6@gmail.com>
2025-02-01 00:43:07 +0000

    Removed the Login and Register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-import LoginPage from "./pages/Login-Page";
-import RegisterPage from "./pages/Register-Page";
-import AgreementPage from './pages/Agreement-Page';
+import AgreementPage from "./pages/Agreement-Page";
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route
-            path='/'
-            element={<LoginPage/>}
-          />
-          <Route
-            path='/registerPage'
-            element={<RegisterPage/>}
-          />
-          <Route
-            path='/loginPage'
-            element={<LoginPage/>}
-          />
-          <Route
-            path='/agreementPage'
-            element={<AgreementPage/>}
-          />
-        </Routes>
-      </Router>
+      <AgreementPage />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
